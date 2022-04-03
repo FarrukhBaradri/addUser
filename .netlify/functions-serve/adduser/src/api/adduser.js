8fca1f895cf66eafc633e0cd52f827c35c176cba
David Bollyeo <david@bollyeo.com>
2022-04-03 17:31:51 +0100

    returning an id need to hook up database next.

--- .netlify/functions-serve/adduser/src/api/adduser.js
+++ .netlify/functions-serve/adduser/src/api/adduser.js
@@ -1,1 +1,23 @@
+var __defProp = Object.defineProperty;
+var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
+var __export = (target, all) => {
+  __markAsModule(target);
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+
+// api/adduser.js
+__export(exports, {
+  handler: () => handler
+});
+async function handler(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ userid: 25 })
+  };
+}
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  handler
+});
 //# sourceMappingURL=adduser.js.map
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,13 @@
 const addUser = async() => {
-    let idField = document.getElementById("userid");
-    idField.innerText = "43";
+    const url = "/.netlify/functions/adduser";
+    const res = await fetch(url);
+    const resJson = await res.json();
+    console.log({resJson});
+
+
+    const userId = resJson.userid;
+    console.log(userId);
+
+    let idField = document.getElementById("useridField");
+    idField.innerText = `${userId}`;
 }
