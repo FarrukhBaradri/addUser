7f28b16776e55e9d3fdc5a7fc54ed35ab743c7c8
FarrukhBaradri <98779711+FarrukhBaradri@users.noreply.github.com>
2022-04-20 11:13:41 +0100

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,7 +2,7 @@ const addUser = async() => {
     const url = "/.netlify/functions/adduser";
     const res = await fetch(url);
     const resJson = await res.json();
-    console.log({resJson});
+    //console.log({resJson});
 
 
     const userId = resJson.userid;
